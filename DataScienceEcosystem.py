9385424cd202bc43182bfde79b93a4aede84716a
JulianAmoah <j.amoah@live.fr>
2024-05-22 22:37:43 +0200

    Document  upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+# 
+
+# In[ ]:
+
+
+from IPython.display import Markdown, display
+
+# Define the markdown content
+markdown_content = "# Data Science Tools and Ecosystem"
+
+# Display the markdown content as a markdown cell
+display(Markdown(markdown_content))
+
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+# 
+
+# **Objectives:**
+# 
+# - List popular languages for Data Science
+# - List commonly used libraries by Data Scientists
+# - Create a table of Data Science tools
+# - Demonstrate examples of evaluating arithmetic expressions in Python
+# 
+
+# ## Author
+# Julian Amoah
+
+# 
+# Some of the popular languages that Data Scientists use are:
+# 
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. Julia
+# 5. Scala
+# 
+
+# 
+# 1. Pandas
+# 2. NumPy
+# 3. Scikit-learn
+# 4. Matplotlib
+# 5. TensorFlow
+# 
+# 
+
+# In[7]:
+
+
+# Define the markdown content
+markdown_content4 = """
+| Data Science Tools       |
+|--------------------------|
+| Jupyter Notebook         |
+| RStudio                  |
+| Apache Zeppelin          |
+"""
+
+# Display the markdown content as a markdown cell
+display(Markdown(markdown_content4))
+
+
+# In[9]:
+
+
+### Below are a few examples of evaluating arithmetic expressions in Python.
+
+
+# In[10]:
+
+
+#This a simple arithmetic expression to mutiply then add integers
+
+(3*4)+5
+
+
+# In[11]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+
+(200/60)
+
+
+# 
